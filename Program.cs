dc3cc7be4635b45b6b5ae62b7268ed016b4854ea
Al Palaran <aljade1902@gmail.com>
2026-07-16 16:40:15 +0100

    remove weather endpoint

--- Program.cs
+++ Program.cs
@@ -4,14 +4,11 @@ using DotNetWebApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services
     .AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -19,28 +16,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-}).WithName("GetWeatherForecast");
-
 app.MapGet("/posts", async () =>
 {
-    HttpClient postsClient = new HttpClient();
-    Post? post = await postsClient.GetFromJsonAsync<Post>(
+    var postsClient = new HttpClient();
+    var post = await postsClient.GetFromJsonAsync<Post>(
         "https://jsonplaceholder.typicode.com/posts/1"
     );
     return (post != null) ? post.Body : "null";
@@ -48,7 +27,7 @@ app.MapGet("/posts", async () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
